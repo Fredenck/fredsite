76d0d4c934c9b8c3c29af6e67a743100e85644b7
Frederick Zhang <frederickzhang@ucla.edu>
2023-07-16 13:46:34 -0700

    revamp home page
    styling: spacing, more text, pretty much finalized for a home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,23 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     // p-24, h-[...] because h-screen conflcits with navbar height and leads to whitespace at the bottom
-    <main className="flex flex-row items-center justify-between p-36 h-[calc(100vh-64px)]">
-      <div className="w-full font-mono basis-4/6">
-        <h1 className="font-bold">Hi, it's Fred</h1>
+    <main className="flex flex-row justify-center h-[calc(100vh-64px)] p-24">
+      <div className="w-full font-mono basis-4/6 pl-12">
+        <h1 className="text-lg font-bold">Hi, it's Fred</h1>
         <br></br>
-        <p className='text-sm'>
-          I've always wanted to make a personal website because it seemed 
-          cool: like a little space for me on the internet to call home. 
-        </p>
+        <br></br>
+        <p className='text-sm'>I've always wanted to make a personal website because it seemed cool.</p>
+        <p className='text-sm'>It'll be like my little space on the internet.</p>
+        <br></br>
+        <br></br>
+        <p className='text-sm'>I have good eyes</p>
+        <p className='text-sm'>but need contacts</p>
+        <br></br>
+        <p className='text-sm'>I am introvert</p>
+        <p className='text-sm'>but extroverted</p>
+        <br></br>
+        <p className='text-sm'>I make good decisions</p>
+        <p className='text-sm'></p>
       </div>
       
       <img src="/aloha.jpg" alt="My face" className="h-full rounded-lg" />
