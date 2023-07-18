e827715e9fe9fde93cee62591536d7de860952c6
Frederick Zhang <frederickzhang@ucla.edu>
2023-07-17 18:05:46 -0700

    casual tone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,9 @@ export default function Home() {
     // p-24, h-[...] because h-screen conflcits with navbar height and leads to whitespace at the bottom
     <main className="flex flex-row justify-center h-[calc(100vh-64px)] p-24">
       <div className="w-full font-mono basis-4/6 pl-12">
-        <h1 className="text-lg font-bold">Hi, it's Fred</h1>
+        <h1 className='text-lg font-bold'>Hey, it's Fred</h1>
         <br></br>
-        <br></br>
-        <p className='text-sm'>I've always wanted to make a personal website because it seemed cool.</p>
-        <p className='text-sm'>It'll be like my little space on the internet.</p>
+        <p className='text-sm' font-bold>This is the fredsite</p>
         <br></br>
         <br></br>
         <p className='text-sm'>I have good eyes</p>
