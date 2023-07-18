f9cacb4f84b3636491fd666ac81513b8f6053e22
Frederick Zhang <frederickzhang@ucla.edu>
2023-07-17 18:50:06 -0700

    navbar cleanup

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -2,35 +2,13 @@
  
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
- 
-export function Navigation({ navLinks }:{navLinks:any}) {
-  const pathname = usePathname()
- 
-  return (
-    <>
-      {navLinks.map((link:any) => {
-        const isActive = pathname.startsWith(link.href)
- 
-        return (
-          <Link
-            className={isActive ? 'text-blue' : 'text-black'}
-            href={link.href}
-            key={link.name}
-          >
-            {link.name}
-          </Link>
-        )
-      })}
-    </>
-  )
-}
 
 export default function Navbar() {
     return (
-        // {Navigation({})}
-        <nav className='flex flex-row items-center mx-auto p-5'>
-            <Link href="/" className="mr-5">Home</Link>
-            <Link href="/blog" className="mr-5">Blog</Link>
-        </nav>
+      // <nav className='flex flex-row justify-end pr-64 py-5 font-mono'>
+      <nav className='flex flex-row justify-center py-5 font-mono'>
+          <Link href="/" className="mx-4">Home</Link>
+          <Link href="/blog" className="mx-4">Blog</Link>
+      </nav>
     )
 }
