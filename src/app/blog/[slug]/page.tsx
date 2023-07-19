0aacead64ba3f805bb8e85cf26f4147e06a52f7e
Frederick Zhang <frederickzhang@ucla.edu>
2023-07-18 18:49:56 -0700

    render markdown with <Mdx>

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,29 @@
+import type { Metadata } from 'next'
 import { format, parseISO } from 'date-fns'
 import { allPosts } from 'contentlayer/generated'
+import { Mdx } from "mdx-components"
+
+// export const metadata: Metadata = {
+//   title: 'Blog',
+//   description: 'My thoughts, interests, and more',
+// };
+
+async function getPostFromParams(params: { slug: string } ) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+
+  if (!post) {
+    null
+  }
+
+  return post
+}
 
 export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
 
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
-  return { title: post.title }
+  return { title: post.title, description: post.description, date: post.date}
 }
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
@@ -14,14 +31,22 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
 
   return (
-    <article className="py-8 pl-96 h-[calc(100vh-64px)]">
+    // <article className="pt-8 px-96">
+    <article className="px-96 pt-8 pb-24">
       <div className="mb-8">
         <h1 className="text-3xl font-bold">{post.title}</h1>
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+        <time dateTime={post.date} className="text-xs mb-1 text-gray-600">
           {format(parseISO(post.date), 'LLLL d, yyyy')}
         </time>
+        {post.description && (
+          <p className="text-xs mt-0 text-slate-700">
+            {post.description}
+          </p>
+        )}
+        <hr className="my-4" />
       </div>
-      <p className='font-serif'>{post.body.raw}</p>
+      <Mdx code={post.body.code} ></Mdx>
+      {/* <p className='font-serif'>{post.body.raw}</p> */}
       {/* <div className="[&>*]:mb-3 [&>*:last-child]:mb-0 font-serif" dangerouslySetInnerHTML={{ __html: post.body.raw }} /> */}
     </article>
   )
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -29,14 +29,9 @@ function PostCard(post: Post) {
 export default async function BlogPage() {
   const posts = allPosts.sort((a:any, b:any) => compareDesc(new Date(a.date), new Date(b.date)))
   return (
-    <div className="py-8 px-96 font-sans h-[calc(100vh-64px)]">
+    <div className="py-8 px-96 font-sans">
       <div className="mb-8">
         <h1 className='text-2xl font-bold'>Blog</h1>
-        <h3>UX inspired by <a href="https://leerob.io/">leerob.io</a></h3>
-        <br></br>
-        <p className='text-sm'>I was always interested in big events of my life, 'canon' events per say. 
-          I first used Instagram to keep track, vaguely, of such events; 
-          however, the caption word limit and image limit of 10 bugged me</p>
       </div>
       <div className=''>
         {posts.map((post, idx) => (
