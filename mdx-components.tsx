adbffca9b730ee785357afad5ffc91c65a4995e8
Frederick Zhang <frederickzhang@ucla.edu>
2023-07-21 23:48:41 -0700

    image hardcoded	fix
    mdx-components is funky	and can't seem to map img: <div><img></img></div> due to a React Hydration Error between initial UI on browser and server rendered UI. Likely nesting a <div> inside a <p>. https://nextjs.org/docs/messages/react-hydration-error. <img> is still really weird and uncenterable with flex justify-content and resizing. flex justify-content works on its own, but once I add w-1/4 it stops centering. Plus, only w-1/4 works; w-1/2, w-1/3, etc just end up to the default... It's likely a contentlayer issue as w-1/3 etc work on my Home Page. So much for cutting-edge modern web-dev :/
    
    I've looked into other resources translating mdx into html and metadata but didn't find anything useful with Nextjs 13's App Router. Stuff like nextra are still based around the Pages Router, although I can probably look into how remark and graymatter interact with the App Router

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -7,7 +7,9 @@ import Link from 'next/link'
 const mdxComponents: MDXComponents = {
     // Override the default <a> element to use the next/link component.
     a: ({ href, children }) => <Link className="underline" href={href as string}>{children}</Link>,
-    img: ({ src, children }) => <img className="justify-center w-1/2 object-scale-down rounded-lg" src={src as string}>{children}</img>,
+    img: ({ src, children }) => <img className="w-1/4 rounded-lg" src={src as string}>{children}</img>,
+    // img: ({ src, children }) => <section className="flex justify-center w-1/4"><img className="rounded-lg" src={src as string}>{children}</img></section>,
+    // Image: ({ src, children }) => <Image className="justify-center w-1/4 object-scale-down rounded-lg" alt="alt" src={src as string}>{children}</Image>,
     h1: (props) => <h1 className="text-2xl" {...props} />,
     h2: (props) => <h2 className="text-xl" {...props} />,
     h3: (props) => <h3 className="text-lg" {...props} />,
