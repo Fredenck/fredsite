abff3c5b76a78f30d41d303f9479d27ce43aa522
Frederick Zhang <frederickzhang@ucla.edu>
2023-08-26 15:20:23 -0700

    closes #5
    add popout icon from feathericons.com, add navbar item, Medal.
    
    Look to standardize navbar items/styles with a component?

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -2,15 +2,26 @@
  
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import Image from 'next/image'
+import Popout from '../public/arrow-up-right.svg'
 
 export const Navbar = () => {
     return (
       // <nav className='flex flex-row justify-end pr-64 py-5 font-mono'>
       <nav className='flex flex-row justify-center py-5 font-mono'>
-          <Link href="/" className="mx-4">Home</Link>
-          <Link href="/projects" className="mx-4">Projects</Link>
-          <Link href="/blog" className="mx-4">Blog</Link>
-          <Link href="https://github.com/Fredenck" target="_blank" className="mx-4">GitHub</Link>
+          <Link href='/' className='mx-4'>Home</Link>
+          <Link href='/projects' className='mx-4'>Projects</Link>
+          <Link href='/blog' className='mx-4'>Blog</Link>
+          <Link href='https://github.com/Fredenck' target='_blank' className='mx-4'>
+            GitHub
+            <Image src={Popout} alt='popout' className='inline -translate-y-1.5 w-3 h-3'/>
+            {/* <span className='material-symbols-outlined'>open_in_new</span> */}
+          </Link>
+          <Link href={'https://medal.tv/u/nutsz'} className='mx-4'>
+            Medal
+            {/* <sup></sup> */}
+            <Image src={Popout} alt='popout' className='inline -translate-y-1.5 w-3 h-3'/>
+          </Link>
       </nav>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ const RootLayout = ({
 }) => {
   return (
     <html lang="en" className={inter.className}>
-      <meta name="google-site-verification" content="aLgGbe1NSIRALGjDoUqY5ZQLutGaTlmPZ-48x3YSp2o" />
+      <head>
+        <meta name="google-site-verification" content="aLgGbe1NSIRALGjDoUqY5ZQLutGaTlmPZ-48x3YSp2o" />
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" /> */}
+      </head>
       <body className='bg-slate-50'>
         <Navbar/>
         {children}
