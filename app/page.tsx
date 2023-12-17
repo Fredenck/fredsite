16665de2776026eda93d9fa561ed9bee1bc80e2d
Frederick Zhang <frederickzhang@ucla.edu>
2023-12-17 00:34:26 -0800

    content+style: revamp home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,19 @@ const Home = () => {
   return (
     // p-24, h-[...] because h-screen conflcits with navbar height and leads to whitespace at the bottom
     <main className="flex flex-col md:flex-row lg:flex-row md:justify-around lg:justify-around h-[calc(100vh-64px)] p-12 md:p-16 lg:p-24">
-      <div className="w-full font-mono basis-2/6">
+      <div className="w-full basis-2/6">
         <h1 className='text-lg font-bold'>Hey, it's Fred</h1>
         <br></br>
-        <p className='text-sm'>This is the fredsite</p>
+        <p>Welcome!</p>
+        <br></br>
+        <p className='text-sm'>This is a mix of cool projects I've done as well as a journal for me. Perhaps I'll separate these as professional/personal in the future.</p>
         <hr className="my-4 w-4/6" />
+        <p className='text-sm'>I made this website with Next 13, Tailwind, and Contentlayer.</p>
         <br></br>
-        <p className='text-sm'>I'm a current UCLA sophomore studying computer science.</p>
+        <p className='text-sm'>I love trying new things! Last quarter I tried our Fencing and Volleyball! This quarter, I plan to continue fencing and get back into Tennis</p>
+        
         <br></br>
-        <p className='text-sm'>Currently, I have a lot of interests. Probably too many. 
-        That's probably why I like computer science; it's applicable to anything</p>
-        <br></br>
-        <p className='text-sm'>You can probably catch me playing volleyball at open gym or fencing practice this quarter!</p>
+        <p className='text-sm'>I also love Computer Science :). It's applicable everywhere and really gives a unique perspective on how you <em>think</em></p>
       </div>
       
       <Image
